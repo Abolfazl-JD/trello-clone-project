e87e8483020d1f4ab46eac12887580fc248f6495
abolfazl-hue <abolfazljdst@gmail.com>
2022-01-05 14:00:49 +0330

    add create task styles and structures

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,28 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue'
+import Vuex from 'vuex'
+import EventHandler from '../services/EventHandler'
 
-Vue.use(Vuex);
+Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    columns: [],
+  },
+  mutations: {
+    ADD_COLUMNS(state, columns) {
+      state.columns = columns
+    },
+  },
+  actions: {
+    getColumns({ commit }) {
+      EventHandler.API_get()
+        .then((res) => {
+          commit('ADD_COLUMNS', res.data)
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    },
+  },
   modules: {},
-});
+})
--- src/views/Board.vue
+++ src/views/Board.vue
@@ -1,42 +1,98 @@
 <template>
   <div class="board">
-    <div class="flex-box">
-      <div class="column">
-        <div class="mb-2 font-weight-bold">todo</div>
-        <div class="list-reset">
+    <div class="flex-box" v-if="columns">
+      <div v-for="(col, colIndex) in columns" :key="colIndex" class="column">
+        <div class="mb-2 font-weight-bold">{{ col.name }}</div>
+        <div
+          v-for="(task, taskIndex) of col.tasks"
+          :key="taskIndex"
+          class="list-reset"
+        >
           <div
-            class="rounded mb-2 pa-3 white grey--text text--darken-2 font-weight-bold"
+            class="rounded mb-2 white grey--text text--darken-2 font-weight-bold"
           >
-            task
-          </div>
-          <div
-            class="rounded mb-2 pa-3 white grey--text text--darken-2 font-weight-bold"
-          >
-            task
-          </div>
-          <div
-            class="rounded mb-2 pa-3 white grey--text text--darken-2 font-weight-bold"
-          >
-            task
+            <v-list-item>
+              <v-list-item-action class="mr-4 mb-4">
+                <v-checkbox v-model="task.done" color="primary"></v-checkbox>
+              </v-list-item-action>
+
+              <v-list-item-content>
+                <v-list-item-title
+                  :class="{
+                    'text-decoration-line-through grey--text': task.done,
+                  }"
+                >
+                  {{ task.name }}
+                </v-list-item-title>
+                <v-list-item-subtitle class="font-italic caption">
+                  {{ task.description }}
+                </v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
           </div>
         </div>
-        <v-text-field
-          outlined
-          dense
-          counter
-          placeholder="+ Enter new task"
-          required
-        ></v-text-field>
+
+        <v-dialog transition="dialog-bottom-transition" max-width="600">
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn class="primary my-2" v-bind="attrs" v-on="on">
+              <v-icon left>mdi-plus</v-icon>
+              create new task
+            </v-btn>
+          </template>
+          <template v-slot:default="dialog">
+            <v-card>
+              <v-card-title class="text-center justify-center mb-7">
+                <h4>Create a new task</h4>
+              </v-card-title>
+              <v-card-text>
+                <v-text-field
+                  outlined
+                  v-model="name"
+                  counter
+                  label="Task to do"
+                ></v-text-field>
+                <v-textarea
+                  label="Add description"
+                  counter
+                  maxlength="35"
+                  clearable
+                  rows="4"
+                  row-height="30"
+                  outlined
+                ></v-textarea>
+              </v-card-text>
+              <v-card-actions class="justify-end">
+                <v-btn color="primary" @click="dialog.value = false"
+                  >Submit</v-btn
+                >
+              </v-card-actions>
+            </v-card>
+          </template>
+        </v-dialog>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
 export default {
   name: 'Home',
 
-  components: {},
+  computed: {
+    ...mapState(['columns']),
+  },
+
+  data() {
+    return {
+      name: '',
+    }
+  },
+
+  mounted() {
+    this.$store.dispatch('getColumns')
+  },
 }
 </script>
 
